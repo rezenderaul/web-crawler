00bc345fe575203e2babd85b4a0c8a78ee3c82b4
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2022-07-06 21:40:06 -0300

    feat: get id and time status of matches

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-function test(number) {
-    return number;
-}
+const puppeteer = require('puppeteer');
 
-test(1);
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized', '--mute-audio'],
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.flashscore.com.br/');
+
+    // accept cookies
+    await (await page.waitForSelector('#onetrust-accept-btn-handler')).click();
+
+    // get all id's
+    const ids = await page.$$eval(
+        '[title="Clique para detalhes do jogo!"]',
+        result => result.map(div => ({
+            id: div.id.substring(4),
+            time: div.children[1].innerText.trim(),
+        })),
+    );
+
+    console.info(ids);
+})();
